5294abc0133a122684a4b9c034e2325f080f16df
ARATA Mizuki <minorinoki@gmail.com>
2010-01-07 11:14:56 +0900

    Added dummy frame offsets to method type encoding

--- test/addmethod_test.lua
+++ test/addmethod_test.lua
@@ -4,18 +4,18 @@ require "cocoa"
 local MyClass = objc.runtime.createClass("MyClass",cocoa.NSObject,{"a","b","c","d"})
 objc.runtime.class_addMethod(MyClass,"test1",function(self,_cmd)
   print("test1:",self,_cmd)
-end,"v@:")
+end,"v0@0:0")
 objc.runtime.class_addMethod(MyClass,"test2:",function(self,_cmd,a)
   print("test2:",self,_cmd,a)
-end,"v@:i")
+end,"v0@0:0i0")
 objc.runtime.class_addMethod(MyClass,"test3::",function(self,_cmd,a,b)
   print("test3:",self,_cmd,a,b)
   return a+b
-end,"f@:ff")
+end,"f0@0:0f0f0")
 objc.runtime.class_addMethod(MyClass,"test4::::::",function(self,_cmd,a,b,...)
   print("test4:",self,_cmd,a,b,...)
   return a+b,a-b,a*b,a/b
-end,"v@:ffo^fo^fo^fo^f")
+end,"v0@0:0f0f0o^f0o^f0o^f0o^f0")
 
 local o = MyClass:alloc():init()
 print("o:",o)
